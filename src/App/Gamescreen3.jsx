37a9ebb48c87ba5a9813a969aa1d2d4d8f1f0525
pY <gustavooliveira2001@live.com>
2024-11-11 00:40:36 -0300

    Adicionando gamescreen3 e tirando a transição de pagina

--- src/App/Gamescreen3.jsx
+++ src/App/Gamescreen3.jsx
@@ -6,33 +6,33 @@ import Header from "../components/Header";
 import BotaoVoltar from "../components/BotaoVoltar";
 import BotaoProximoPasso from "../components/BotaoProximoPasso";
 
-export default function GameScreem2() {
-  const [nomeChave, setNomeChave] = useState("");
-  const [listaChaves, setListaChaves] = useState([]);
+export default function GameScreem3() {
+  const [nomeTime, setNomeTime] = useState("");
+  const [listaTimes, setListaTimes] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
 
-  const handleEnviarChave = () => {
-    if (nomeChave.trim() !== "") {
+  const handleEnviarTime = () => {
+    if (nomeTime.trim() !== "") {
       if (editIndex !== null) {
-        const updatedChaves = listaChaves.map((chave, index) =>
-          index === editIndex ? nomeChave : chave
+        const updatedTimes = listaTimes.map((time, index) =>
+          index === editIndex ? nomeTime : time
         );
-        setListaChaves(updatedChaves);
+        setListaTimes(updatedTimes);
         setEditIndex(null);
       } else {
-        setListaChaves([...listaChaves, nomeChave]);
+        setListaTimes([...listaTimes, nomeTime]);
       }
-      setNomeChave("");
+      setNomeTime("");
     }
   };
 
-  const handleDeleteChave = (index) => {
-    const updatedChaves = listaChaves.filter((_, i) => i !== index);
-    setListaChaves(updatedChaves);
+  const handleDeleteTime = (index) => {
+    const updatedTimes = listaTimes.filter((_, i) => i !== index);
+    setListaTimes(updatedTimes);
   };
 
-  const handleEditChave = (index) => {
-    setNomeChave(listaChaves[index]);
+  const handleEditTime = (index) => {
+    setNomeTime(listaTimes[index]);
     setEditIndex(index);
   };
 
@@ -44,30 +44,30 @@ export default function GameScreem2() {
         <div className="flex justify-center mt-4 space-x-1">
           <div className="w-3 h-3 bg-custom-green-1 rounded-full"></div>
           <div className="w-3 h-3 bg-custom-green-1 rounded-full"></div>
-          <div className="w-3 h-3 bg-custom-green-2 rounded-full"></div>
+          <div className="w-3 h-3 bg-custom-green-1 rounded-full"></div>
           <div className="w-3 h-3 bg-custom-green-2 rounded-full"></div>
           <div className="w-3 h-3 bg-custom-green-2 rounded-full"></div>
         </div>
 
         <div className="flex justify-center mt-10">
           <h1 className="text-3xl font-bold text-custom-green-2">
-            Chaveamento
+            Times
           </h1>
         </div>
 
         <div className="flex flex-col items-center mt-10">
           <div className="w-5/6">
             <label
-              htmlFor="nomeChave"
+              htmlFor="nomeTime"
               className="text-2xl font-bold mb-2 text-custom-green-2"
             >
-              Nome da Chave (ex: Chave A):
+              Adicionar Time:
             </label>
             <input
-              id="nomeChave"
+              id="nomeTime"
               type="text"
-              value={nomeChave}
-              onChange={(e) => setNomeChave(e.target.value)}
+              value={nomeTime}
+              onChange={(e) => setNomeTime(e.target.value)}
               placeholder="Escreva Aqui!"
               className="w-full p-3 border-2 mt-3 border-custom-green-1 rounded-3xl focus:outline-none focus:border-custom-green-1 text-lg"
             />
@@ -77,33 +77,33 @@ export default function GameScreem2() {
         <div className="flex justify-center mt-6">
           <button
             className="text-custom-green-1 bg-white focus:outline-none font-extrabold rounded-3xl text-lg px-6 py-3 transition-transform duration-200 hover:scale-105 active:scale-95 hover:text-custom-green-2 hover:border-custom-green-2 border-2 border-custom-green-1 shadow-md"
-            onClick={handleEnviarChave}
+            onClick={handleEnviarTime}
           >
-            {editIndex !== null ? "Editar Chave" : "Enviar Chave"}
+            {editIndex !== null ? "Editar Time" : "Enviar Time"}
           </button>
         </div>
 
         <div className="flex flex-col items-center mt-10">
           <h2 className="text-3xl font-bold text-custom-green-2">
-            Lista de Chaves
+            Lista de Times
           </h2>
           <ul className="w-5/6 mt-4">
-            {listaChaves.map((chave, index) => (
+            {listaTimes.map((time, index) => (
               <li
                 key={index}
                 className="flex items-center justify-between p-4 mb-4 bg-custom-green-1 text-white text-lg rounded-xl"
               >
-                <span className="flex-grow text-left font-bold">{chave}</span>
+                <span className="flex-grow text-left font-bold">{time}</span>
                 <div className="flex space-x-4">
                   <FontAwesomeIcon
                     icon={faPen}
                     className="cursor-pointer text-blue-500 text-xl"
-                    onClick={() => handleEditChave(index)}
+                    onClick={() => handleEditTime(index)}
                   />
                   <FontAwesomeIcon
                     icon={faTrashCan}
                     className="cursor-pointer text-red-500 text-xl"
-                    onClick={() => handleDeleteChave(index)}
+                    onClick={() => handleDeleteTime(index)}
                   />
                 </div>
               </li>
@@ -111,7 +111,7 @@ export default function GameScreem2() {
           </ul>
         </div>
       </div>
-      <BotaoProximoPasso nextPage="/gamescreen3" />
+      <BotaoProximoPasso nextPage="/" />
       <Footer />
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
+
+// Páginas
 import LandingPage from './App/LandingPage';
 import RegisterPage from './App/RegisterPage';
+import LoginPage from './App/LoginPage';
 import DashBoard from './App/Dashboard';
 import GameScreem1 from './App/GameScreem1';
-import GameScreem2 from './App/Gamescreen2'
-import LoginPage from './App/LoginPage';
+import GameScreem2 from './App/Gamescreen2';
+import GameScreem3 from './App/Gamescreen3';
+
+// Contexto
 import { UserProvider } from './UserContext';
+
+// Componentes
 import ProtectedRoute from './components/ProtectedRoute';
-import PageTransition from './components/Transition';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserProvider>
       <Router>
-        <PageTransition>
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/register" element={<RegisterPage />} />
@@ -25,8 +30,8 @@ root.render(
             <Route path="/dashboard" element={<ProtectedRoute element={<DashBoard />} />} />
             <Route path="/gamescreen1" element={<GameScreem1 />} />
             <Route path="/gamescreen2" element={<GameScreem2 />} />
+            <Route path="/gamescreen3" element={<GameScreem3 />} />
           </Routes>
-        </PageTransition>
       </Router>
     </UserProvider>
   </React.StrictMode>
