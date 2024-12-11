bf5323591fac0e00c1f80f1321669b35ce9cb011
pY <gustavooliveira2001@live.com>
2024-12-11 02:07:31 -0300

    Arrumando links e adicionando botao voltar e titulo

--- src/App/Historico.jsx
+++ src/App/Historico.jsx
@@ -49,9 +49,12 @@ const Historico = () => {
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
+  
+  console.log('loading:', loading);
+  console.log('error:', error);
 
   const handleCopyLink = (linkAcesso) => {
-    const link = `http://localhost:3000/campeonatos/public/${linkAcesso}`;
+    const link = `http://localhost:3001/jogos/${linkAcesso}`;
     navigator.clipboard.writeText(link)
       .then(() => alert("Link copiado com sucesso!"))
       .catch(() => alert("Erro ao copiar o link."));
